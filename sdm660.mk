1d4a52925fa29878ab1b016d5ae0178196af2945
OdSazib <odsazib@gmail.com>
2021-06-06 19:09:23 +0600

    sdm660-common: Update RIL from LA.UM.9.11.r1-03900-NICOBAR.0 * Telephony from LA.QSSI.11.0.r1-11600-qssi.0
    * Changes to Radio and telephony:
    - Update android.hardware.radio to 1.5
    - Update vendor.qti.hardware.radio.uim_remote_client to 1.2
    - Update vendor.qti.data.factory to 2.2
    - Update vendor.qti.hardware.radio.ims to 1.7
    - Add manifest entry for vendor.qti.hardware.mwqemadapter
    - Add seccomp_policy for imsrtp
    - Remove duplicate vendor.qti.hardware.radio.qtiradio entry
    - Remove libshim for libdpmframework
    - Adjust props with new RIL blobs
    
    Signed-off-by: OdSazib <odsazib@gmail.com>
    Change-Id: Ib1dd70c8ac029c929d973d62fe69b2b9bc38d119

--- sdm660.mk
+++ sdm660.mk
@@ -484,7 +484,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
+    android.hardware.radio@1.5 \
     android.hardware.radio@1.2 \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio@1.4.vendor \
@@ -510,6 +510,7 @@ PRODUCT_BOOT_JARS += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/seccomp/imsrtp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/imsrtp.policy \
     $(COMMON_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(COMMON_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
@@ -541,10 +542,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel
-
-# Shims
-PRODUCT_PACKAGES += \
-    libcutils_shim
 
 # Tetheroffload
 PRODUCT_PACKAGES += \
